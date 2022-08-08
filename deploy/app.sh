c300b7d7e1c435e59d02703085898d80089b22d5
Alexsong623 <Alexsong623@hotmail.com>
2022-08-09 00:06:00 +0800

    Fix deploy sh bug on host.

--- deploy/app.sh
+++ deploy/app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 pip3 install -r /bootroomchat/soccerdataanalysis/requirements.txt
 pip3 install --no-cache-dir -r /bootroomchat/soccerdataanalysis/big-requirements.txt
-uvicorn /bootroomchat/soccerdataanalysis/webapp/main:app --reload --port 8811
+uvicorn /bootroomchat/soccerdataanalysis/webapp/main:app --reload --port 8811 -host 0.0.0.0
