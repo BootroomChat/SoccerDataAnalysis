26d1876a1e715fc836bf315ee2e6563cce162cd8
Alexsong623 <Alexsong623@hotmail.com>
2022-08-09 09:14:28 +0800

    Deploy api service.

--- deploy/app.sh
+++ deploy/app.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
-pip3 install -r /bootroomchat/soccerdataanalysis/requirements.txt
-pip3 install --no-cache-dir -r /bootroomchat/soccerdataanalysis/big-requirements.txt
-uvicorn /bootroomchat/soccerdataanalysis/webapp/main:app --reload --port 8811 -host 0.0.0.0
+#pip3 install -r /bootroomchat/soccerdataanalysis/requirements.txt
+#pip3 install --no-cache-dir -r /bootroomchat/soccerdataanalysis/big-requirements.txt
+cd  /bootroomchat/soccerdataanalysis/webapp
+BUILD_ID=DONTKILLME
+ps -ef |grep uvicorn |awk '{print $2}'|xargs kill -9
+nohup uvicorn main:app --port 8811 --host 0.0.0.0 &
+exit
