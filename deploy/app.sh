d3d971624e9bf5788fae89e7d84b044873f8b04e
Alexsong623 <Alexsong623@hotmail.com>
2022-08-10 15:57:43 +0800

    Add soccer data pkg test.

--- deploy/app.sh
+++ deploy/app.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
-#pip3 install -r /bootroomchat/soccerdataanalysis/requirements.txt
-#pip3 install --no-cache-dir -r /bootroomchat/soccerdataanalysis/big-requirements.txt
-cd  /bootroomchat/soccerdataanalysis/webapp
 BUILD_ID=DONTKILLME
+pip3 install -r /bootroomchat/soccerdataanalysis/requirements.txt
+# pip3 install --no-cache-dir -r /bootroomchat/soccerdataanalysis/big-requirements.txt
+cd  /bootroomchat/soccerdataanalysis/webapp
 ps -ef |grep uvicorn |awk '{print $2}'|xargs kill -9
 nohup uvicorn main:app --port 8811 --host 0.0.0.0 &
 exit
