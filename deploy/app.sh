1a4dda7547353dd8a950f11382d65632f0568a7f
Alexsong623 <Alexsong623@hotmail.com>
2022-10-03 20:09:40 +0800

    Add crontab task for match batches.

--- deploy/app.sh
+++ deploy/app.sh
@@ -6,4 +6,5 @@ cd  /bootroomchat/soccerdataanalysis
 nohup python3.8 -m batch.send_report > test.out 2>&1 &
 ps -ef |grep uvicorn |awk '{print $2}'|xargs kill -9
 nohup python3.8 -m uvicorn webapp.main:app --port 8811 --host 0.0.0.0 > test.out 2>&1 &
+crontab /bootroomchat/soccerdataanalysis/cron.d/batch.cron
 exit
